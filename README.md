07ee23222dbe3f9712031f58a31b512593c77a83
telekrex <telekrex@gmail.com>
2023-04-06 22:20:47 -0400

    neatened

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ⚡ Velocity
 **[WIP]** A sandbox for ideation and iteration on small electric powertrain designs.
 
-See source/notepad.txt for my latest scratch which is ahead of the actual code.
+See notepad.txt for my latest scratch which is ahead of the actual code.
--- source/formulas.py
+++ source/formulas.py
@@ -20,3 +20,22 @@ def nm_to_ftlbs(nm):
 
 def solve_for_hp(rpm, torque):
 	return rpm * torque / 5252
+
+
+
+
+
+
+def solve_for_time(distance, speed):
+	# time = distance / speed
+	return ''
+
+
+def solve_for_speed(distance, time):
+	# speed = distance / time
+	return ''
+
+
+def solve_for_distance(speed, time):
+	# distance = speed * time
+	return ''
